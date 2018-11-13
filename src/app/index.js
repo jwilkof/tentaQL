e8edb22d93bdf48c1897bfd8d6a4559bd7a8f671
filmboy3 <jonathanschwartz30@gmail.com>
2018-11-13 16:25:33 -0500

    boilerplate redux setup

--- src/app/index.js
+++ src/app/index.js
@@ -3,16 +3,39 @@ const ReactDOM = require("react-dom");
 import {Component} from "react";
 require("./index.css");
 import Navbar from "./components/Navbar"
+import { createStore, compose } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+
+const initialState = {};
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
 import TextBox from "./components/TextBox"
+import Footer from './components/Footer'
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList'
 import {Button, Icon, Input, Checkbox, Form, Menu} from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
-        <TextBox/>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Navbar/>
+          <AddTodo />
+          <VisibleTodoList />
+          <Footer />
+          <TextBox/>
+        </div>
+      </Provider>
     );
   }
 }
